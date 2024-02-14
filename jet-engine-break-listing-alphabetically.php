da54927e355eeef5666be3b95e0024864e7df404
Andrew Shevchenko <andremjhead@gmail.com>
2024-02-14 19:12:45 +0200

    UPD: replace accents

--- jet-engine-break-listing-alphabetically.php
+++ jet-engine-break-listing-alphabetically.php
@@ -178,8 +178,10 @@ class Jet_Engine_Break_Listing_Alphabetically {
 		}
 
 		$prop = mb_substr( $object->$prop, 0, 1 );
-		$prop = iconv( 'UTF-8','ASCII//TRANSLIT', $prop );
-		$prop = preg_replace( '/[^a-zA-Z]*/', '', $prop );
+
+		$accents_map = apply_filters( 'jet-engine-break-alphabetically/accents-map', [ 'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' ] );
+		
+		$prop = isset( $accents_map[ $prop ] ) ? $accents_map[ $prop ] : $prop;
 
 		return strtoupper( $prop );
 
